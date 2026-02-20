1706b1f81c6fddce9a47ed05a770604de1a26dfd
vahenikoghosyan <vahenikoghosyan@doublecoconut.com>
2026-02-21 02:39:39 +0400

    chore: changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/layout/Header/Header'
-import ProductListPage from './pages/ProductListPage'
-import ProductDetailsPage from './pages/ProductDetailsPage'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header/Header";
+import ProductListPage from "./components/pages/ProductListPage/ProductListPage";
+import ProductDetailsPage from "./components/pages/ProductDetailsPage/ProductDetailsPage";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-      <Route path="/" element={<ProductListPage />} />
-      <Route path="/category/:id" element={<ProductListPage />} />
-      <Route path="/product/:id" element={<ProductDetailsPage />} />
+        <Route path="/" element={<ProductListPage />} />
+        <Route path="/category/:id" element={<ProductListPage />} />
+        <Route path="/product/:id" element={<ProductDetailsPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/ProductDetailsPage.tsx
+++
@@ -1,1 +0,0 @@
-export { default } from './components/pages/ProductDetailsPage/ProductDetailsPage'
